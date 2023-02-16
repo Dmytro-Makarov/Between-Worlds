e2c13080f89c4698f6e0262a1e5fd5ce903da4c6
Dmytro-Makarov <makarowdoua@gmail.com>
2023-02-16 13:46:37 +0200

    Initial Commit --MC Animations --Basic Collissions --TileMap in progress

---
+++ Player/Player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+const ACCELERATION = 500
+const MAX_SPEED = 125
+const FRICTION = 600
+
+@onready var animationPlayer = $AnimationPlayer
+@onready var animationTree = $AnimationTree
+@onready var animationState = $AnimationTree.get("parameters/playback")
+
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+		
+	else:
+		animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		
+	move_and_slide()
