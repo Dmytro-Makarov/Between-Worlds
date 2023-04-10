6be33ebfbc3f625d21c8561fe88403cc5157de11
Dmytro-Makarov <makarowdoua@gmail.com>
2023-04-10 23:01:40 +0300

    renaming to PascalCase

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const PlayerHurtSound = preload("res://Player/player_hurt_sound.tscn")
+const PlayerHurtSound = preload("res://Player/PlayerHurtSound.tscn")
 
 @export var ACCELERATION = 500
 @export var MAX_SPEED = 110
