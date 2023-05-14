d04a1fe36144812f4515c27fb9ab4f40b2217dd4
Dmytro-Makarov <makarowdoua@gmail.com>
2023-05-14 14:51:00 +0300

    sidescroll progress
    not much
    god help us all

--- Player/Player.gd
+++ Player/Player.gd
@@ -43,9 +43,8 @@ func _physics_process(delta):
 
 func move_state(delta):
 	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	input_vector = input_vector.normalized()
+	input_vector.x = Input.get_axis("left","right")
+	input_vector.y = Input.get_axis("up","down")
 	
 	if input_vector != Vector2.ZERO:
 		roll_vector = input_vector
