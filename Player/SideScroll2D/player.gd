2b2ad6a1133cb5bfc7a3ee2853a15e472f48a9e6
Dmytro-Makarov <makarowdoua@gmail.com>
2023-05-20 21:00:15 +0300

    animation work

--- Player/SideScroll2D/player.gd
+++ Player/SideScroll2D/player.gd
@@ -1,13 +1,26 @@
 extends CharacterBody2D
 
+#TODO: To mirror the character,
+#Use flip_h for the sprite,
+#and scale property for collision shape
 
 @export var speed : float = 200.0
-@export var jump_velocity : float = -150.0
-@export var double_jump_velocity : float = -125.0
+@export var jump_velocity : float = -175.0
+@export var double_jump_velocity : float = -150.0
+
+@onready var animation_tree = $AnimationTree 
+@onready var animation_state = $AnimationTree.get("parameters/playback")
+
+@onready var idle_sprite = $SpriteNode/IdleSprite
+@onready var run_sprite = $SpriteNode/RunSprite
+@onready var jump_sprite = $SpriteNode/JumpSprite
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var has_double_jumped : bool = false
+var direction: Vector2 = Vector2.ZERO
+
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -24,15 +37,29 @@ func _physics_process(delta):
 		elif not has_double_jumped:
 			#Double jump in air
 			#+= adds momentum in immediate jump
-			velocity.y += double_jump_velocity
+			velocity.y = double_jump_velocity
 			has_double_jumped = true
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("left", "right")
+	direction = Input.get_vector("left", "right", "up", "down")
 	if direction:
-		velocity.x = direction * speed
+		velocity.x = direction.x * speed
+		scale.x = scale.y * direction.x
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
+	_update_animation()
+
+func _update_animation():
+	idle_sprite.visible = false
+	run_sprite.visible = false
+	jump_sprite.visible = false
+	
+	if direction.x != 0:
+		animation_state.travel("Run")
+		run_sprite.visible = true
+	else:
+		animation_state.travel("Idle");
+		idle_sprite.visible = true
